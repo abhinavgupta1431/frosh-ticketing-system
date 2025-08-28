0bb9e59d61cbe6ada57fd8ad9cdbdfb3fddc2ee9
Abhinav Gupta <abhinavgupta1431@gmail.com>
2025-08-28 22:43:03 +0530

    Add files via upload

---
+++ frosh_web/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for frosh_web project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from apps.users import views as user_views
+from apps.events import views as event_views
+
+urlpatterns = [
+   path('', user_views.home),
+   path('events/', include("apps.events.urls")),
+   path('bablucopter/', admin.site.urls),
+   path('bablucopter/login', user_views.login_user),
+   path('login/', user_views.login_user),
+   path('logout/', user_views.logout_user),
+   path('', include("apps.users.urls")),
+   path('users/', include("django.contrib.auth.urls")),
+   path('scanner/', include("apps.validation.urls")),
+   path('hoods/', include("apps.hoods.urls")),
+   path('hijack/', include('hijack.urls')),
+]
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
